585738743319af11f53d847f2cbd7f320dfc6153
Magwit <magnushakon@hotmail.com>
2020-06-24 15:11:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options)
+# driver.get("https://repl.it/@SergeiChestakov/selenium")
+driver.get("http://www.karjalainen.fi/")
+time.sleep(3)
+driver.close()
+driver.quit()
