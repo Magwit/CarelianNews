e922977293d48c112ff0ef88a0c19edc39bcab70
Magwit <magnushakon@hotmail.com>
2020-06-24 15:15:27 +0000

    import pytest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import pytest
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
