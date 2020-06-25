84524a52d0f52f9d77edd9f0b1ecbfc73b4b6030
Magwit <magnushakon@hotmail.com>
2020-06-25 14:42:27 +0000

    add theme locator

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
-import pytest
+# import pytest
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -21,14 +21,14 @@ hamburger = driver.find_element_by_id("offcanvas-toggler")
 
 hamburger.click()
 time.sleep(1)
-full_screen_themes_xpath = "//a[text()='Teemat']"
-small_screen_themes_xpath = "//li/a[@href='/teemat']"
-cheat_themes_xpath = "/html/body/div[7]/div/div[1]/div/ul/li[4]/a"
+# full_screen_themes_xpath = "//a[text()='Teemat']"
+# small_screen_themes_xpath = "//li/a[@href='/teemat']"
+# cheat_themes_xpath = "/html/body/div[7]/div/div[1]/div/ul/li[4]/a"
 
 time.sleep(1)
 
 # themes = driver.find_element_by_xpath(cheat_themes_xpath)
-themes = driver.find_element_by_link_text('Teemat')
+themes = driver.find_element_by_link_text('TEEMAT')
 themes.click()
 time.sleep(2)
 # Idea . test using different breakpoints
